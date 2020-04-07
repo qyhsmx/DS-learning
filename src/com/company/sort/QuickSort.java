74bbe9974eb274767b25848cc4e35cf52eb1f17a
qinyong <1069542931@qq.com>
2020-04-07 20:50:58 +0800

    quick sort optimization

--- src/com/company/sort/QuickSort.java
+++ src/com/company/sort/QuickSort.java
@@ -47,10 +47,13 @@ public class QuickSort {
 
     private static void quick_sort(int[] arr,int low,int high){
         int pivot;
-        if(low < high){
+        while (low < high){
             pivot = partition(arr,low,high);
-            quick_sort(arr,0,pivot-1);
-            quick_sort(arr,pivot+1,high);
+            quick_sort(arr,low,pivot-1);
+            //quick_sort(arr,pivot+1,high);
+            // can be instead of tail recurse
+            low = pivot+1;
+
         }
     }
 
